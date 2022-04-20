e9ae4d7442febb06a973b2daa7295eb08fc282ce
futuran <takuya.kudryavka@gmail.com>
2022-04-20 22:32:23 +0900

    add src or trg only

---
+++ util-NeuralBoW/01.preprocess_NeuralBoW_trgonly.sh
@@ -0,0 +1,19 @@
+# XLM-RoBERTaに基づくフレーズ抽出モデル訓練のためのデータセット作成
+CORPUS=$1
+DIR=../../data/${CORPUS}
+
+mkdir -p ${DIR}.sbert_trgonly.oracle
+mkdir -p ${DIR}.sbert_trgonly.NBoW
+
+for tvt in dev test train_h40000
+do
+    python 01.preprocess_NeuralBoW.py  -src            ${DIR}.sbert_trgonly/merge.top10/${CORPUS}_$tvt.en.with_match.tkn \
+                                    -ref            ${DIR}.sbert_trgonly/merge.top10/${CORPUS}_$tvt.ja.with_match.tkn \
+                                    -out_in4oracle  ${DIR}.sbert_trgonly.oracle/${CORPUS}_$tvt.oracle.src.tkn \
+                                    -out_ref4oracle ${DIR}.sbert_trgonly.oracle/${CORPUS}_$tvt.oracle.trg.tkn \
+                                    -out_in4nbow    ${DIR}.sbert_trgonly.NBoW/${CORPUS}_$tvt.in4nbow \
+                                    -out_ref4nbow   ${DIR}.sbert_trgonly.NBoW/${CORPUS}_$tvt.ref4nbow \
+                                    -out_numofsim   ${DIR}.sbert_trgonly.NBoW/${CORPUS}_$tvt.numofsim
+done
+
+
---
+++ util-retrieve/02.search_by_faiss_trgonly.sh
@@ -0,0 +1,24 @@
+CORPUS=$1
+DIR=../../data/${CORPUS}
+
+mkdir -p ${DIR}.sbert_trgonly/match
+# 2 search by faiss
+for prefix in dev test train_h40000
+do
+    if [ $prefix = 'test' ]; then
+        PM='--include-perfect-match'
+    else
+        PM=''
+    fi
+
+    # 検索対象は原言語側のみ
+    python 02.search_by_faiss.py \
+        -q    ${DIR}.sbert/emb/${prefix}.ja.emb \
+        -qt   ${DIR}/${CORPUS}_$prefix.en.tkn \
+        -tms  ${DIR}.sbert/emb/train.ja.emb \
+        -tmst ${DIR}/${CORPUS}_train.en.tkn \
+        -o    ${DIR}.sbert_trgonly/match/${prefix}.match \
+        -k 10 -d 768 $PM
+    wait
+done
+
